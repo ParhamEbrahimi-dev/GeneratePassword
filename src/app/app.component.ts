1eff46a73e065584223474043344ff9c111570a5
Parham Ebrahimi <ParhamEbrahimi.dev@outlook.com>
2024-12-19 14:52:57 +0330

    FirstCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,8 @@
+import {NgModule} from '@angular/core';
+@NgModule({
+  // Metadata goes here
+})
+export class CustomMenuModule { }
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -8,5 +13,61 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'GeneratePassword';
+  password:string="";
+
+  lettersCheck:boolean=false;
+  symbolsCheck:boolean=false;
+  numbersCheck:boolean=false;
+  length:number=0;
+
+  constructor ()
+  {
+  }
+  getPassword():string
+  {
+    return this.password;
+  }
+  OnButtonClick()
+  {
+    let numbers ="1234567890"; 
+    let letters ="asdfghjklqwertyuio"; 
+    let symbols ="!@#$%^&*()"; 
+
+    let validChar="";
+    if(this.numbersCheck)
+      validChar += numbers;
+
+    if(this.lettersCheck)
+      validChar += letters;
+
+    if(this.symbolsCheck)
+      validChar += symbols;
+
+    let generatedPass= "";
+    for(let i:number=0; i<this.length;i++)
+    {
+      let index=Math.floor(Math.random()*validChar.length);
+      generatedPass+=validChar[index];
+    }
+
+    this.password=generatedPass;
+ 
+
+  }
+  OnChangeLetters(){
+    this.lettersCheck=!this.lettersCheck;
+  }
+  OnChangeNumbers(){
+    this.numbersCheck=!this.numbersCheck;
+  }
+  OnChangeSymbols(){
+    this.symbolsCheck=!this.symbolsCheck;
+  }
+  onChangeInput(value:string){
+    const changedLength=parseInt(value)
+    if(!isNaN(changedLength))
+      this.length=changedLength;
+    else
+    this.length=0;
+  }
 }
